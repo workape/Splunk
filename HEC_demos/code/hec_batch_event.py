ec4b891542feb907a9fb4a40b00ff54b10f556ef
Tighe Schlottog <workape@gmail.com>
2023-09-26 07:57:18 -0700

    Removed old IP Address

--- HEC_demos/code/hec_batch_event.py
+++ HEC_demos/code/hec_batch_event.py
@@ -5,7 +5,7 @@ urllib3.disable_warnings()
 
 blah=['{"event": {"src": "de:ad:be:ef:00:00", "dot11_subtype": "beacon", "bssid": "00:00:de:ad:be:ef", "privacy": "WPA2", "rssi": -49, "dst": "ff:ff:ff:ff:ff:ff", "channel": 6, "ssid": "funtimes"}, "sourcetype": "dot11_mgmt", "time": 1582785809.000000}','{"event": {"src": "de:ad:be:ef:00:00", "dot11_subtype": "beacon", "bssid": "00:00:de:ad:be:ef", "privacy": "WPA2", "rssi": -49, "dst": "ff:ff:ff:ff:ff:ff", "channel": 6, "ssid": "funtime"}, "sourcetype": "dot11_mgmt", "time": 1582785810.0000}']
 
-SPLUNK_INSTANCE = "172.16.12.131"
+SPLUNK_INSTANCE = "IP_ADDRESS_HERE"
 HEC_TOKEN = 'bc3c577b-3015-4f91-b333-364d0ee75a57'
 HEC_PORT = '8088'
 
